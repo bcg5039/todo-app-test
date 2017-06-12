d5e4b8fa269d04efae54bdfe4327207342372ed7
bcg5039 <bcg5039@gmail.com>
2017-06-12 15:00:26 -0600

    check working

--- cmds/check.js
+++ cmds/check.js
@@ -3,7 +3,31 @@ const ls = require('./ls.js')
 const {map} = require('ramda')
 
 module.exports = function(id){
-const list = get()
+
+  // ['3']    // 1 == '1' true
+  const todoId = parseInt(id[0], 10)
+  // todoId = 3
+
+  const list = get()
+  const newTodos = map(function (todo) {
+    if (todo.id === todoId) {
+      todo.completed = !todo.completed
+    }
+    return todo
+  }, list.todos)
+  set({counter: list.counter, todos: newTodos})
+  return ls();
+}
+
+  //const newTodos = map(ifIdMatchesTodoIdThenMarkComplete, list)
 
 
+
+function ifIdMatchesTodoIdThenMarkComplete (id) {
+  return function (todo) {
+    if (todo.id === id) {
+      todo.completed = !todo.completed
+    }
+    return todo
+  }
 }
